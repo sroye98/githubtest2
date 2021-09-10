482ef107110aae47a5cac186deb847ffdf5397f8
Saachi Roye <saachi.roye@gmail.com>
2021-09-10 16:27:05 -0500

    Update WeatherForecastController.cs

--- githubtest2.webapi/Controllers/WeatherForecastController.cs
+++ githubtest2.webapi/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace githubtest2.webapi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Sunny", "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
